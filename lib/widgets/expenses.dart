c7bd38610bc4ff610c9edec20c3475b7ccba8be5
Erik Kronquist <erik.kronquist@ucdenver.edu>
2025-09-07 18:11:59 -0600

    End Lecture 109

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_app/models/expense.dart';
 import 'package:expense_tracker_app/widgets/expenses_list/expenses_list.dart';
+import 'package:expense_tracker_app/widgets/new_expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -30,7 +31,7 @@ class _ExpensesState extends State<Expenses> {
   void _openAddExpenseOverlay() {
     showModalBottomSheet(
       context: context,
-      builder: (ctx) => Text('Modal Bottom Sheet'),
+      builder: (ctx) => NewExpense(),
     );
   }
 
