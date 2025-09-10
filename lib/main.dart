e09f165fff66a9f41c751e077b81676e6898dcf2
Erik Kronquist <erik.kronquist@ucdenver.edu>
2025-09-10 13:12:48 -0600

    End Lecture 138

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 
 import 'package:expense_tracker_app/widgets/expenses.dart';
 
@@ -13,10 +13,10 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]).then((fn) {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // SystemChrome.setPreferredOrientations([
+  //   DeviceOrientation.portraitUp,
+  // ]).then((fn) {
     runApp(
       MaterialApp(
         darkTheme: ThemeData.dark().copyWith(
@@ -65,5 +65,5 @@ void main() {
         home: const Expenses(),
       ),
     );
-  });
+  // });
 }
--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -67,6 +67,8 @@ class _ExpensesState extends State<Expenses> {
 
   @override
   Widget build(context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text('No expenses found. Start adding some!'),
     );
@@ -88,9 +90,16 @@ class _ExpensesState extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
+      body: width < 600 ? Column(
         children: [
           Chart(expenses: _registeredExpenses),
+          Expanded(
+            child: mainContent,
+          ),
+        ],
+      ) : Row(
+        children: [
+          Expanded(child: Chart(expenses: _registeredExpenses)),
           Expanded(
             child: mainContent,
           ),
